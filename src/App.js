b0f9c3ff79dfd1ae5011b1acc055ca4b9d0dcaa8
Prachi <sachdevaprachi6@gmail.com>
2023-03-19 23:14:15 +0530

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
-
-
 import Cart from './Cart';
+import Navbar from './Navbar';
 function App() {
   return (
     <div className="App">
    <h1>cart</h1>
+   <Navbar/>
    <Cart/>
     </div>
   );
