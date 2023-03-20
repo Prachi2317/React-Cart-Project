47639bd1cf323cf3a95383e08814d64a83c85f81
Prachi <sachdevaprachi6@gmail.com>
2023-03-20 11:19:01 +0530

    name CartItem a function component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 // this is a class based component
 // creating a class cartItem that inherits the properties from component class of react library
-class CartItem extends React.Component{
+const CartItem =(props)=>{
    // when i am inheriting something from parent class then you need to call the  constructor of parent class and (super) call the constructor of parent class
     
   
@@ -40,10 +40,9 @@ class CartItem extends React.Component{
     //     }
     //   });
     // }
-  render(){
     // object destructuring
-    const{price,title,qty}=this.props.product;
-    const{product,onIncreaseQuantity,onDecreaseQuantity,onDeleteProduct}=this.props
+    const{price,title,qty}=props.product;
+    const{product,onIncreaseQuantity,onDecreaseQuantity,onDeleteProduct}=props
     return(
         <div className="cart-item">
             <div className="left-block">
@@ -55,15 +54,15 @@ class CartItem extends React.Component{
                 <div style={{color:'blue'}}>{qty}</div>
                 <div className="cart-item-action">
                     {/* Buttons */}
-                    <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/3524/3524388.png" onClick={()=>onIncreaseQuantity(this.props.product)} />
-                    <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" onClick={()=>onDecreaseQuantity(this.props.product)} />
+                    <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/3524/3524388.png" onClick={()=>onIncreaseQuantity(props.product)} />
+                    <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" onClick={()=>onDecreaseQuantity(props.product)} />
                     <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png" onClick={()=>onDeleteProduct(product.id)} />
 
                 </div>
             </div>
         </div>
     );
-  }
+  
 }
 //creating a style object
 const styles={
