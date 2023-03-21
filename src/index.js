ef902c49c014f8c7e64e9cd4d20b30e33ee353bb
Prachi <sachdevaprachi6@gmail.com>
2023-03-21 09:44:27 +0530

    made changes in firebase setup file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore/lite';
+import * as firebase from "firebase/app"
+import 'firebase/firestore'
 const firebaseConfig = {
   apiKey: "AIzaSyBpGn49bEBd5nLwve4s3O3ecxl0CfBYi7A",
   authDomain: "cart-407d8.firebaseapp.com",
@@ -12,11 +12,11 @@ const firebaseConfig = {
   messagingSenderId: "527567310169",
   appId: "1:527567310169:web:7ec861f9a4f9584f8e9048"
 };
-// Initialize Firebase
-const firebaseApp=initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
 
-export default db;
+// Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
